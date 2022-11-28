32362636245ba24cd7ae55b8f4b90fec4c8f89fe
Chandran_98 <chandranjc25@gmail.com>
2022-11-28 18:06:27 +0530

    df

--- contracts/nft_marketplace.sol
+++ contracts/nft_marketplace.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
 import "hardhat/console.sol";
 
 contract KFT is ERC721URIStorage {
@@ -32,11 +31,15 @@ contract KFT is ERC721URIStorage {
         uint256 indexed tokenid,
         address sender,
         address owner,
-        uint price,
+        uint256 price,
         bool issold
     );
     modifier onlyOwner() {
-        require(address(this) == owner, "Only owner can access it");
+        require(msg.sender == owner, "Only owner can access it");
+        _;
+    }
+    modifier baseprice() {
+        require(msg.value == listingprice, "Price must be listing price");
         _;
     }
 
@@ -48,17 +51,113 @@ contract KFT is ERC721URIStorage {
         listingprice = _listingprice;
     }
 
-    function getlisting() public view returns (uint256) {
-        return listingprice;
+    function Createnft(
+        string calldata tokenuri,
+        uint256 amount
+    ) public payable returns (uint256) {
+        _tokenid.increment();
+        uint newtokenid = _tokenid.current();
+
+        _mint(msg.sender, newtokenid);
+        _setTokenURI(newtokenid, tokenuri);
+        createmarketitem(newtokenid, amount);
+
+        return newtokenid;
     }
 
-    function mintnft(string memory tokenuri, uint256 price)
-        public
-        returns (uint256)
-    {
-        _tokenid.increment();
-        uint256 newtokenid = _tokenid.current();
-        _min
+    // createmarketitem function is used to create nft item by user and transfer the nft from seller to contract
+    function createmarketitem(
+        uint256 tokenid,
+        uint256 amount
+    ) private baseprice {
+        require(amount == 0, "Price required");
 
+        idToMarketItem[tokenid] = Marketitematy(
+            tokenid,
+            payable(msg.sender),
+            payable(address(this)),
+            amount,
+            false
+        );
+
+        //     transfer the nft from seller to contract
+        _transfer(msg.sender, address(this), tokenid);
+
+        emit infoMarketitem(
+            tokenid,
+            payable(msg.sender),
+            payable(address(this)),
+            amount,
+            false
+        );
+    }
+
+    // resellnft is used to update the price in existing nft.
+    function resellnft(uint tokenid, uint amount) public payable baseprice {
+        require(
+            idToMarketItem[tokenid].owner == msg.sender,
+            "You must be the owner of the nft"
+        );
+        idToMarketItem[tokenid].issold == false;
+        idToMarketItem[tokenid].price == amount;
+        idToMarketItem[tokenid].seller == msg.sender;
+        idToMarketItem[tokenid].owner == address(this);
+
+        _itemssold.decrement();
+
+        _transfer(msg.sender, address(this), amount);
+        emit infoMarketitem(
+            tokenid,
+            payable(msg.sender),
+            payable(address(this)),
+            amount,
+            false
+        );
+    }
+
+    /// sale nft is used for product sales in market and to transfer the price to owner wallet.
+    function salenft(uint256 tokenid) public payable {
+        uint256 price = idToMarketItem[tokenid].price;
+        require(msg.value == price, "Please check the price amount");
+        idToMarketItem[tokenid].owner = payable(msg.sender);
+        idToMarketItem[tokenid].issold = true;
+        idToMarketItem[tokenid].owner = payable(address(0));
+
+        _itemssold.increment();
+
+        _transfer(address(this), msg.sender, tokenid);
+        payable(owner).transfer(listingprice);
+        payable(idToMarketItem[tokenid].seller).transfer(msg.value);
+    }
+
+    // Unsold nft listing
+
+    function unsoldnft() public view returns (Marketitematy[] memory) {
+        uint256 itemcount = _tokenid.current();
+        uint256 unsoldnftcount = _tokenid.current() - _itemssold.current();
+        uint256 currentindex = 0;
+        Marketitematy[] memory items = new Marketitematy[](unsoldnftcount);
+        for (uint256 i = 0; i < itemcount; i++) {
+            if (idToMarketItem[i + 1].owner == address(this)) {
+                uint256 currentid = i + 1;
+                Marketitematy storage currentitem = idToMarketItem[currentid];
+                items[currentindex] = currentitem;
+                currentindex += 1;
+            }
+        }
+        return items;
+    }
+
+    // Fetch our own NFT collection
+
+    function fetchnft() public view returns (Marketitematy[] memory) {
+        uint256 totalcount = _tokenid.current();
+        uint256 currentindex = 0;
+        Marketitematy[] memory items = new Marketitematy[](totalcount);
+        for (uint i = 0; i < currentindex; i++) {
+            if (idToMarketItem[i + 1].owner == address(this)) {
+                uint256 currentid = i + 1;
+            }
+        }
     }
 }
